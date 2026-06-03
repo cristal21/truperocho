336ea590cd80428d0ac88f157939ed56f7fc22c9
cristal21 <105411124+cristal21@users.noreply.github.com>
2026-06-02 20:58:30 -0600

    Cambie productos

--- productos.php
+++ productos.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Truper - Productos</title>
+    <title>Truper - PRODUCTOS</title>
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body { font-family: Arial, sans-serif; background: #f4f4f4; }
